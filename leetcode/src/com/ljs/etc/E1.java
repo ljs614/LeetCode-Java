1072bbc26b95609a2f7faaed41cda7fad96fbc93
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-06-01 15:16:08 +0900

    Add permutation

--- leetcode/src/com/ljs/etc/E1.java
+++ leetcode/src/com/ljs/etc/E1.java
@@ -1,46 +1,53 @@
 package com.ljs.etc;
 
+import java.util.*;
+
 public class E1 {
-// 재귀라는데 ....
-//	input 3
-//	output 123
-//	       132
-//	       213
-//	       231
-//	       312
-//	       321
-	
-	
-	public void solution(int n) {
-		
-		int[] arr = new int[n];
-		
-		for(int i=0; i<n; i++) {
+	// 재귀라는데 ....
+	//	input 3
+	//	output 123
+	//	       132
+	//	       213
+	//	       231
+	//	       312
+	//	       321
+	public void solution(int num) {
+
+		int[] arr= new int[num];
+		for(int i=0; i<num; i++){
 			arr[i]=i+1;
 		}
-		
-		for(int i=0; i<n; i++) {
-			String[] strArr = permutation(i+1, arr);
-			for(String s : strArr) {
-				System.out.println(arr[i]+s);
-			}
+		List<String> list = permutation(arr);
+		for(String s : list){
+			System.out.println(s);
+
 		}
-		
 	}
-	
-	public String[] permutation(int n, int[] arr){
+
+
+	public List<String> permutation(int[] arr)  {
 		int LEN = arr.length;
-		String[] strArr = new String[LEN-1];
-		int[] temp = new int[LEN-1];
-		int index = 0;
-		for(int i=0; i<LEN; i++) {
-			if(i+1 != n) {
-				temp[index++] = arr[i];
+		List<String> list = new ArrayList<>();
+		for(int i=0; i<LEN; i++){
+			int index=0;
+			int[] numArr = new int[LEN-1];
+			for(int j=0; j<LEN;j++){
+				if(arr[i]!=arr[j]){
+					numArr[index++]=arr[j];
+				}}
+			if(LEN-1>1){
+				List<String> temp= permutation(numArr);
+				for(String s : temp){
+					list.add(arr[i]+s);
+				}
+			} else{
+				list.add(""+arr[i]+numArr[0]);
 			}
+
 		}
-		for(int i = 0; i<temp.length; i++) {
-		}
-		return strArr;
-	}
-	
+		return list;
+	}                    
+
+
 }
+
